acfe7528616aa1e724e234a0cd164774c1b9833a
leekiye99JHTA <ousterss@gmail.com>
2024-08-14 14:27:25 +0900

    feat. Hr Controller Update

---
+++ src/main/java/com/yojulab/study_springboot/dao/HRDao.java
@@ -0,0 +1,17 @@
+package com.yojulab.study_springboot.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HRDao {
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    public Object createEmpAttendance(String email, String date) {
+
+    }
+
+
+}
