8147b8a0632095f49e9e64beabaef6a1e6259ae9
wonc <wonclee112@gmail.com>
2024-08-14 13:03:31 +0900

    1

---
+++ src/main/java/com/yojulab/study_springboot/hr/DepartmentService.java
@@ -0,0 +1,28 @@
+package com.yojulab.study_springboot.hr;
+
+import com.yojulab.study_springboot.dao.SharedDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    SharedDao sharedDao;
+
+    public Object insert(Map dataMap) {
+        String sqlMapId = "Department.insert";
+        Object result = sharedDao.insert(sqlMapId, dataMap);
+        return result;
+    }
+
+
+    public Object delete(Map dataMap) {
+        String sqlMapId = "Department.delete";
+
+        Object result = sharedDao.delete(sqlMapId, dataMap);
+        return result;
+    }
+}
