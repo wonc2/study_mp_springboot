49ca0af88d3a25e946eb9902fcf86a9cbe681783
wonc <wonclee112@gmail.com>
2024-08-14 14:57:10 +0900

    add getList?

---
+++ src/main/java/com/yojulab/study_springboot/service/AttendanceService.java
@@ -0,0 +1,25 @@
+package com.yojulab.study_springboot.service;
+
+import com.yojulab.study_springboot.dao.SharedDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Transactional
+@Service
+public class AttendanceService {
+
+    @Autowired
+    SharedDao sharedDao;
+
+    public Map selectListByDepartment(Map dataMap) {
+        String sqlMapId = "Attendance.selectListByDepartment";
+
+        HashMap result = new HashMap<>();
+        result.put("resultList", sharedDao.getList(sqlMapId, dataMap));
+        return result;
+    }
+}
